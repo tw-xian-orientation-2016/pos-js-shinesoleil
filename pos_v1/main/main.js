f0f168d704d67a40110100b05b0d1d01d18511bb
shinesoleil <hao_pan@outlook.com>
2016-02-25 12:08:36 +0800

    getBarcodeQuantity done

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,1 +1,25 @@
-//TODO: Please write code in this file.
+function getBarcodeQuantity(inputs) {
+  var inputsInOrder = inputs.sort();
+  var barcodes = {};
+
+  for(var i=0; i<inputsInOrder.length; i++) {
+    var barcode = inputsInOrder[i];
+    var number;
+
+    if(barcode.indexOf('-') !== -1) {
+      number = parseInt(barcode.split('-')[1]);
+      barcode = barcode.split('-')[0];
+    }
+    else {
+      number = 1;
+    }
+
+    if(!barcodes[barcode]) {
+      barcodes[barcode] = number;
+    }
+    else {
+      barcodes[barcode] += number;
+    }
+  }
+  return barcodes;
+}
