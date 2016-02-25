ebe124a0afdca625f4c42f960420ccb03380e0ff
shinesoleil <hao_pan@outlook.com>
2016-02-25 12:09:18 +0800

    getBarcoeQuantity-test bug fixed

--- pos_v1/test/getBarcodeQuantity-test.js
+++ pos_v1/test/getBarcodeQuantity-test.js
@@ -11,22 +11,14 @@ describe('getBarcodeQuantity test', function() {
     'ITEM000005'
   ];
 
-  outputs = [
-    {
-      'barcode':'ITEM000001',
-      'count': 5
-    },
-    {
-      'barcode':'ITEM000003',
-      'count': 2
-    },
-    {
-      'barcode':'ITEM000005',
-      'count': 3
-    }
-  ];
+  outputs =
+  {
+    'ITEM000001': 5,
+    'ITEM000003': 2,
+    'ITEM000005': 3
+  };
 
   it('counts the quantity of every item ', function() {
-    expect(gerBarcodeQuantity(inputs)).toBe(outputs);
+    expect(getBarcodeQuantity(inputs)).toEqual(outputs);
   })
 });
