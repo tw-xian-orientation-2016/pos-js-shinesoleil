c551766fb02a030f601a7d1bd8415436acb716c6
shinesoleil <hao_pan@outlook.com>
2016-02-25 11:23:45 +0800

    getBarcodeQuantity test done

--- pos_v1/test/getBarcodeQuantity-test.js
+++ pos_v1/test/getBarcodeQuantity-test.js
@@ -1,3 +1,32 @@
-/**
- * Created by hao on 2/25/16.
- */
+describe('getBarcodeQuantity test', function() {
+  inputs = [
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'
+  ];
+
+  outputs = [
+    {
+      'barcode':'ITEM000001',
+      'count': 5
+    },
+    {
+      'barcode':'ITEM000003',
+      'count': 2
+    },
+    {
+      'barcode':'ITEM000005',
+      'count': 3
+    }
+  ];
+
+  it('counts the quantity of every item ', function() {
+    expect(gerBarcodeQuantity(inputs)).toBe(outputs);
+  })
+});
